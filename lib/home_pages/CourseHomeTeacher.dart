49025bec08c8290d0ed4b1c547c5cd5369ba4472
fahad <fahadkamalzahrani14@gmail.com>
2024-02-14 12:59:44 +0000

    dialog for number of tasks

--- lib/home_pages/CourseHomeTeacher.dart
+++ lib/home_pages/CourseHomeTeacher.dart
@@ -21,31 +21,11 @@ class _CourseTeacherState extends State<CourseTeacher> {
     names[index] = name;
   }
 
-  Color colouring(int numberOfTasks, int index){
-    if (numberOfTasks == 1) {
-      return Colors.blue.shade100 ;
-    }
-    else {
-      return randomColor(index~/numberOfTasks);
-    }
 
-  }
 
-  Color randomColor(int index){
-    List<Color> colors = [
-      Color(0xFF3F51B5), // indigo
-      Color(0xFF4CAF50), // green
-      Color(0xFF2196F3), // blue
-      Color(0xFFE91E63), // pink
-      Color(0xFFFFC107), // amber
-      Color(0xFFFF5722), // deep orange
-      Color(0xFF795548), // brown
-      Color(0xFF9C27B0), // purple
-    ];
 
-    return colors[index % colors.length];
-  }
-  void showTasksDialogue(){
+  void showTasksDialogue(BuildContext context){
+    int numberOfTasks = 1;
     showDialog(context: context, builder: (BuildContext context){
       return AlertDialog(
         title: Text("Assign Tasks"),
@@ -87,7 +67,6 @@ class _CourseTeacherState extends State<CourseTeacher> {
         ],
       );
     });
-
   }
 
 
